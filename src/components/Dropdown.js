3bf4c69f2a48a290d97d545a7686333ed87b6e35
Aliza Qamer <aqamer.bese19seecs@seecs.edu.pk>
2021-09-17 22:56:13 +0500

    button color change

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -84,7 +84,7 @@ export default function Dropdown({isOpen,toggle}) {
             ))}
                 </Dropdownmenu>
                 <BtnWrapper>
-                    <Button big="true" primary="true" to="/contact">Contact Me</Button>
+                    <Button big="true" primary="true" to="/contact" style={{color:"white"}}>Contact Me</Button>
                 </BtnWrapper>
             </DropdownWrapper>
         </Dropdowndiv>
